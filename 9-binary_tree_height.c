18954ed67b6993c3889c8ed99b2c597b4ad67efa
MoamenZyan <moamenzyan20@gmail.com>
2024-02-27 00:49:22 +0200

    9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * recurison_func - recurison function for binary_tree_height.
+ * @tree: current node.
+ * @height: current height.
+ * Return: height of the tree.
+ */
+
+size_t recurison_func(const binary_tree_t *tree, size_t height)
+{
+	if (!tree)
+		return (height + 0);
+	height++;
+
+	size_t left_height = recurison_func(tree->left, height);
+	size_t right_height = recurison_func(tree->right, height);
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+
+}
+/**
+ * binary_tree_height - function that calculate height of the tree.
+ * @tree: root of the tree
+ * Return: height of the tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (recurison_func(tree, -1));
+}
