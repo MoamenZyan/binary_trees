284a85e03932abe2391b52570bc1ab9bb2c0d9a1
MoamenZyan <moamenzyan20@gmail.com>
2024-02-27 16:43:27 +0200

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,11 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if (!new || !parent)
+	if (new == NULL)
 		return (NULL);
 
 
