15c64b490b379feba7339d6b01dea6a7d232ee41
MoamenZyan <moamenzyan20@gmail.com>
2024-02-29 13:58:18 +0200

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Function To Get Depth Of A Node
+ * @tree: The Node.
+ * Return: Depth Of The Node.
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
