36afb567010f59163b7f127c956989cac7e39335
MoamenZyan <moamenzyan20@gmail.com>
2024-02-29 13:08:49 +0200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,18 +9,29 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	size_t left_height, right_height;
+
+	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
+	if (tree->parent == NULL)
 	{
-		return (left_height + 1);
+		binary_tree_depth(tree->left);
+		binary_tree_depth(tree->right);
+	}
+	else if (tree->left != NULL || tree->right != NULL)
+	{
+		left_height = binary_tree_depth(tree->left) + 1;
+		right_height = binary_tree_depth(tree->right) + 1;
 	}
 	else
 	{
-		return (right_height + 1);
+		left_height = 0;
+		right_height = 0;
 	}
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
