7df7cf72793c3bbc5c821cda38ba38b38d042bee
MoamenZyan <moamenzyan20@gmail.com>
2024-02-29 14:21:56 +0200

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,30 +11,31 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left_size, right_size;
 
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		if (tree->parent == NULL)
+		{
+			left_size = binary_tree_size(tree->left);
+			right_size = binary_tree_size(tree->right);
+		}
+			left_size = binary_tree_size(tree->left) + 1;
+			right_size = binary_tree_size(tree->right) + 1;
 
-	if (tree->parent == NULL)
-	{
-		left_size = binary_tree_size(tree->left);
-		right_size = binary_tree_size(tree->right);
-	}
-		left_size = binary_tree_size(tree->left) + 1;
-		right_size = binary_tree_size(tree->right) + 1;
-
-	if (tree->parent != NULL)
-	{
-		if (left_size > right_size)
-			return (left_size);
+		if (tree->parent != NULL)
+		{
+			if (left_size > right_size)
+				return (left_size);
+			else
+				return (right_size);
+		}
 		else
-			return (right_size);
+		{
+			if (left_size > right_size)
+				--left_size;
+			else
+				--right_size;
+			return (left_size + right_size);
+		}
 	}
-	else
-	{
-		if (left_size > right_size)
-			--left_size;
-		else
-			--right_size;
-		return (left_size + right_size);
-	}
+	return (0);
 }
